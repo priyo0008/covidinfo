bc2367fb8ea0c2ab4f75dcadb173095e33005b74
priyo0008 <priyansh0008@gmail.com>
2020-06-08 14:41:05 +0530

    Updates

---
+++ precache-manifest.8eaaf04c5301fc4f090aff77a0be746a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df32b897e9862caa187195d42c1ee212",
+    "url": "/%7Breact-deploye%7D/index.html"
+  },
+  {
+    "revision": "d8aa0423493f062b49c8",
+    "url": "/%7Breact-deploye%7D/static/css/main.94675e02.chunk.css"
+  },
+  {
+    "revision": "ac38de5dc60f84d5b829",
+    "url": "/%7Breact-deploye%7D/static/js/2.3512f179.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/%7Breact-deploye%7D/static/js/2.3512f179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8aa0423493f062b49c8",
+    "url": "/%7Breact-deploye%7D/static/js/main.8596acd5.chunk.js"
+  },
+  {
+    "revision": "4c9159b57bae31953307",
+    "url": "/%7Breact-deploye%7D/static/js/runtime-main.eb530eec.js"
+  },
+  {
+    "revision": "73abf0126abe075d13d9bc7873690a36",
+    "url": "/%7Breact-deploye%7D/static/media/horsegif.73abf012.gif"
+  },
+  {
+    "revision": "49b2d2489232401c4a6699616ed38517",
+    "url": "/%7Breact-deploye%7D/static/media/priyansh.49b2d248.jpeg"
+  },
+  {
+    "revision": "61cbe55385557bdfe88d3bb538955d5f",
+    "url": "/%7Breact-deploye%7D/static/media/undercon.61cbe553.png"
+  }
+]);
